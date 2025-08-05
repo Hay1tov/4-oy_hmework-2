9c8e527a0f8b6ba5efddb9d1f94eb0377e0d3fb1
Hay1tov <eldorh35@gmail.com>
2025-08-05 14:59:32 +0500

    fixed

--- app.js
+++ app.js
@@ -16,20 +16,18 @@
 //                                  No_2
 
 
-// function mean(a, b) {
+// const mean = (function(a, b) {
 //   let result = (a + b) / 2;
 //   let result2 = Math.sqrt(a * b);
 //   console.log(`O'rta arimtetigi ${result}`);
 //   console.log(`O'rta geometrigi ${result2}`);
-// }
-
-// mean(12, 48)
+// })(12, 48)
 
 
 //                                  No_3
 
 
-// function sign(n) {
+// const sign = n => {
 //   if (n > 0) {
 //     console.log(`Natija 1`);
 //   } else if (n < 0) {
@@ -45,7 +43,7 @@
 //                                  No_4
 
 
-// function numberOfRoots(A, B, C) {
+// const numberOfRoots = (function(A, B, C) {
 //   let D = B ** 2 - 4 * A * C;
 //   if (D > 0) {
 //     console.log(`Natija 2`);
@@ -54,27 +52,23 @@
 //   } else {
 //     console.log(`Natija 0`);
 //   }
-// }
-
-// numberOfRoots(-1, -6, 9);
+// })(-1, -6, 9)
 
 
 //                                  No_5
 
 
-// function areaCircle(R) {
+// const areaCircle = (function(R) {
 //   let S = Math.PI * R ** 2;
 
 //   console.log(`Natija ${S.toFixed(2)}`);
-// }
-
-// areaCircle(20)
+// })(20)
 
 
 //                                  No_6
 
 
-// function sumRange(A, B) {
+// const sumRange = (function(A, B) {
 //   let result = 0;
 //   if (A <= B) {
 //     for (let i = A; i <= B; i++) {
@@ -84,16 +78,13 @@
 //   } else {
 //     return 0;
 //   }
-// }
-
-
-// sumRange(8, 1);
+// })(8, 10)
 
 
 //                                  No_7
 
 
-// function calc(A, B, S) {
+// const calc = (function(A, B, S) {
 //   let result = 0;
 //   switch (S) {
 //     case '+':
@@ -115,15 +106,13 @@
 //     default:
 //       console.log(`Natija 0`)
 //   }
-// }
-
-// calc(4,8,'/');
+// })(4,8,'/')
 
 
 //                                  No_8
 
 
-// function isEven(K) {
+// const isEven = K => {
 //   if (K % 2 == 0) {
 //     console.log(true);
 //   } else {
@@ -137,7 +126,7 @@
 //                                  No_9
 
 
-// function sortABC(a, b, c) {
+// const sortABC = (function(a, b, c) {
 //   let min = null;
 //   let mid = null;
 //   let max = null;
@@ -172,15 +161,13 @@
 //   }
 
 //   console.log(`Natija ${min} ${mid} ${max}`);
-// }
-
-// sortABC(10, 5, 8);
+// })(10, 5, 8)
 
 
 //                                  No_10
 
-
-// function isPowerN(K, N) {
+ 
+// const isPowerN = function(K, N) {
 //   if (K <= 0 || N <= 1) {
 //     return 0;
 //   }
@@ -198,7 +185,7 @@
 //                                  No_11
 
 
-// function isPrime(N) {
+// const isPrime = (function(N) {
 //   let loop = 0;
 //   if (N < 2) {
 //     return false;
@@ -213,9 +200,7 @@
 //   } else {
 //     console.log(false);
 //   }
-// }
-
-// isPrime(4) // N > 0 shart bo'yicha son kiriting
+// })(4)  // N > 0 shart bo'yicha son kiriting
 
 
 //                                  No_12
@@ -223,7 +208,7 @@
 
 // let tub = 0;
 
-// function isPrime(N) {
+// const isPrime = function(N) {
 //   let loop = 0;
 //   if (N < 2) {
 //     return false;
@@ -240,7 +225,7 @@
 //   }
 // }
 
-// function numberOfPrime(number) {
+// const numberOfPrime = function(number) {
 //   let count = 0;
 //   for (let j = 1; j <= number; j++) {
 //     if (isPrime(j)) {
@@ -256,11 +241,9 @@
 //                                  No_13
 
 
-// function digitCount(K) {
-//   return K.toString().length;
-// }
+// const digitCount = K => K.toString().length;
 
-// function digitNth(K, N) {
+// const digitNth = (function(K, N) {
 //   let count = digitCount(K);
 
 //   if (N > count) {
@@ -268,9 +251,9 @@
 //   }
 
 //   return parseInt(K.toString()[N - 1]);
-// }
+// })(105782, 5)
 
-// console.log(digitNth(105782, 5));
+// console.log(digitNth);
 
 
 //                                  No_14
